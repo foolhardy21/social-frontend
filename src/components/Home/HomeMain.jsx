a41e6056c228ca8b2fffe7328aa5ee7a4201af13
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-04-10 16:19:00 +0530

    feat - home page ui components added

---
+++ src/components/Home/HomeMain.jsx
@@ -0,0 +1,30 @@
+import styles from './home.module.css'
+import { useNavigate } from 'react-router-dom'
+
+const HomeMain = () => {
+    const navigate = useNavigate()
+
+    const navigateToSignup = () => {
+        navigate('/signup')
+    }
+
+    const navigateToLogin = () => {
+        navigate('/login')
+    }
+
+    return (
+        <main className='flx flx-column pd-md'>
+
+            <p className='txt-secondary txt-xxlg txt-500'>sneaker culture</p>
+
+            <p className='txt-secondary txt-xlg txt-400 mg-top-xs mg-btm-xxlg'>join the gang</p>
+
+            <button onClick={navigateToSignup} className={`btn-solid ${styles.btnSignup} bg-primary txt-primary txt-lg txt-400 pd-top-s pd-btm-s brd-s mg-top-md mg-btm-md`}>sign up</button>
+
+            <button onClick={navigateToLogin} className='btn-outlined b-solid b-secondary txt-secondary txt-lg txt-400 pd-top-s pd-btm-s brd-s'>log in</button>
+
+        </main>
+    )
+}
+
+export default HomeMain
---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import logo from '../assets/sneakerhood-logos_white.png'
+import { HomeMain } from '../components/Home'
+
+const Home = () => {
+
+    return (
+        <div
+            style={{
+                minHeight: '100vh',
+            }}
+            className='flx flx-center bg-off-secondary'
+        >
+            <img srcSet={logo} style={{
+                width: '50vw'
+            }} />
+
+            <HomeMain />
+
+        </div>
+    )
+}
+
+export default Home
