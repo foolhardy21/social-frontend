b218715af98879d246b744adabad6abb6b758649
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-04-10 13:53:11 +0530

    feat - signup page ui components added

---
+++ src/components/Signup/SignupCard.jsx
@@ -0,0 +1,16 @@
+import { SignupForm } from "./"
+
+const SignupCard = () => {
+
+    return (
+        <section className="bg-primary pd-xlg brd-s">
+
+            <p className="txt-xlg txt-primary txt-400">create your account</p>
+
+            <SignupForm />
+
+        </section>
+    )
+}
+
+export default SignupCard
