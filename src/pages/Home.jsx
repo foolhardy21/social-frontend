e3b63d59dfccebebcf70c5c57b5b76fa8384619d
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-04-17 11:19:22 +0530

    refactor - css class for hero image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import logo from '../assets/sneakerhood-logos_white.png'
 import { HomeMain } from '../components/Home'
+import styles from '../components/Home/home.module.css'
 
 const Home = () => {
 
@@ -10,9 +11,8 @@ const Home = () => {
             }}
             className='flx flx-center bg-off-secondary'
         >
-            <img srcSet={logo} style={{
-                width: '50vw'
-            }} />
+            <img srcSet={logo} className={styles.heroImg}
+            />
 
             <HomeMain />
 
